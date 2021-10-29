efe079a18bcc5cb59e063d20d95b2cebb91ec239
onyegood <goodmomen@gmail.com>
2021-10-29 11:21:53 +0100

    Added validation to user resolvers

--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -1,14 +1,14 @@
 import { __prod__ } from "./constants";
-import { Post } from "./entities/Post";
 import {MikroORM} from '@mikro-orm/core'
 import path from 'path'
+import { User, Post } from "./entities";
 
 export default {
     migrations: {
         path: path.join(__dirname, './migrations'),
         pattern: /^[\w-]+\d+\.tj$/,
     },
-    entities: [Post],
+    entities: [Post, User],
     dbName: 'lireddit',
     type: 'postgresql',
     debug: !__prod__
--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -1,6 +1,8 @@
 import { User } from '../entities';
-import {Arg, Ctx, Query, Resolver, Mutation, InputType, Field} from 'type-graphql';
+import {Arg, Ctx, Resolver, Mutation, InputType, Field, ObjectType} from 'type-graphql';
 import { MyContext } from 'src/types';
+import argon2 from 'argon2';
+import { FieldError } from 'src/entities/Error';
 
 @InputType()
 class UsernamePasswordInput {
@@ -10,16 +12,85 @@ class UsernamePasswordInput {
     password: string
 }
 
-
+@ObjectType()
+class UserResponse {
+    @Field(() => [FieldError], {nullable: true})
+    errors?: FieldError[]
+    
+    @Field(() => User, {nullable: true})
+    user?: User
+}
 
 @Resolver()
 export class UserResolvers {
-    @Mutation(() => User)
+    @Mutation(() => UserResponse)
     async createUser(
         @Arg('options') options: UsernamePasswordInput,
-        @Ctx() { em }: MyContext): Promise<User> {
-        const user = em.create(User, { username: options.username });
-        await em.persistAndFlush(user);
-        return user;
+        @Ctx() { em }: MyContext): Promise<UserResponse> {
+        if (options.username.length <= 4) {
+            return {
+                errors: [{
+                    field: 'username',
+                    message: "username must not be less than 5 characters length"
+                }]
+            }
+        }
+
+        if (options.password.length <= 7) {
+            return {
+                errors: [{
+                    field: 'password',
+                    message: "password must not be less than 8 characters length"
+                }]
+            }
+        }
+        
+        // Encript password using argon2 
+        const hashedPassword = await argon2.hash(options.password);
+
+        const user = em.create(User, { username: options.username, password: hashedPassword });
+        try {
+            await em.persistAndFlush(user);
+        } catch (error) {
+            // duplicate username error
+            if (error.code === '23505' || error.detail.includes("already exists")) {
+                return {
+                    errors: [{
+                        field: 'username',
+                        message: "username already taken"
+                    }]
+                }
+            }
+        }
+        return { user };
+    }
+
+    @Mutation(() => UserResponse)
+    async login(
+        @Arg('options') options: UsernamePasswordInput,
+        @Ctx() { em }: MyContext): Promise<UserResponse> {
+        const user = await em.findOne(User, { username: options.username });
+
+        if (!user) {
+            return {
+                errors: [{
+                    field: 'username',
+                    message: "that username doesn't exist"
+                }]
+            }
+        }
+
+        // Verify encripted password 
+        const valid = await argon2.verify(user.password, options.password);
+        if (!valid) {
+            return {
+                errors: [{
+                    field: 'password',
+                    message: "incorrect password"
+                }]
+            }
+        }
+        
+        return { user };
     }
 }
