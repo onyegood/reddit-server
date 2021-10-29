4da389ee9cad4491761825dfc52aace304b6e023
onyegood <goodmomen@gmail.com>
2021-10-29 14:34:51 +0100

    Setting up redis and session storage

--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,61 @@
-import 'reflect-metadata';
-import {MikroORM} from '@mikro-orm/core';
+import "reflect-metadata";
+import { MikroORM } from "@mikro-orm/core";
+import redis from "redis";
+import session from "express-session";
+import connnectRedis from "connect-redis";
+import { __prod__ } from "./constants";
+import express from "express";
+import microConfig from "./mikro-orm.config";
+import { ApolloServer } from "apollo-server-express";
 
-import { __prod__ } from './constants';
-import express from 'express';
-import microConfig from './mikro-orm.config';
-import {ApolloServer} from 'apollo-server-express';
-
-import {buildSchema} from 'type-graphql';
-import { PostResolvers } from './resolvers/post';
-
+import { buildSchema } from "type-graphql";
+import { PostResolvers, UserResolvers } from "./resolvers";
 
 const main = async () => {
     const orm = await MikroORM.init(microConfig);
-    // Run DB Migration
+  // Run DB Migration
     await orm.getMigrator().up();
 
-    // Create Web Server
+  // Create Web Server
     const app = express();
     const port = process.env.PORT || 4000;
-    
+
+    const RedisStore = connnectRedis(session);
+    const redisClient = redis.createClient();
+
+    app.use(
+        session({
+            name: "qid",
+            store: new RedisStore({ 
+                client: redisClient,
+                disableTouch: true
+            }),
+            cookie: {
+                maxAge: 1000 * 60 * 60 * 24 * 365 * 10, // 10 Years
+                httpOnly: true,
+                secure: __prod__ // Cookie only works in https
+            },
+            secret: 'process.env.SESSION_SECRET_KEY',
+            resave: false,
+        })
+    );
+
     const apolloServer = new ApolloServer({
         schema: await buildSchema({
-            resolvers: [PostResolvers],
-            validate: false
+            resolvers: [PostResolvers, UserResolvers],
+            validate: false,
         }),
-        context: () => ({ em: orm.em })
+        context: () => ({ em: orm.em }),
     });
 
-    await apolloServer.start()
-    apolloServer.applyMiddleware({ app })
+    await apolloServer.start();
+    apolloServer.applyMiddleware({ app });
 
     app.listen(port, () => {
-        console.log(`server started on localhost:${port}`)
-    })
-}
+        console.log(`server started on localhost:${port}`);
+    });
+};
 
-main().catch(err => {
+main().catch((err) => {
     console.log(err);
 });
