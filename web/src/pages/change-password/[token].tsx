c7f678490b5f9f0830036cb19a687daa4820bdcc
onyegood <goodmomen@gmail.com>
2021-11-02 14:04:56 +0100

    Started working on change password frontend

---
+++ web/src/pages/change-password/[token].tsx
@@ -0,0 +1,11 @@
+import { NextPage } from 'next';
+import React from 'react';
+
+
+const ChangePassword: NextPage<{token: string}> = ({}) => {
+    return (
+        <p>Hello bro</p>
+    )
+}
+
+export default ChangePassword;
